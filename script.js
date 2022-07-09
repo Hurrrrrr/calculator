90a9a1cebcfba68632567236b62ad62790ab7081
Hurrrrrr <jonahpalmer@gmail.com>
2022-07-09 15:57:25 -0400

    start calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
+function operate(operator, first, second) {
+    switch(operator) {
+        case "+":
+            return add(first, second);
+        case "-":
+            return subtract(first, second);
+        case "*":
+            return multiply(first, second);
+        case "/":
+            return divide(first, second);
+    }
+}
